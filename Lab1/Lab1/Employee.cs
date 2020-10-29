32a645b041d0caaafcf4ef79786e0e01cadc7b28
Wiles_Joshua <Wiles_Joshua@edu.mcctc.net>
2020-10-29 13:35:01 -0400

    Done

--- Lab1/Lab1/Employee.cs
+++ Lab1/Lab1/Employee.cs
@@ -3,15 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+using System.IO;
 
 namespace Lab1
 {
-    [Serializable]
+    [XmlRoot("New Employees", Namespace = "www.nerdinc.ninja")]
     public class Employee
     {
+        [XmlAttribute("Employee ID")]
         public int ID { get; set; }
+        [XmlElement("Full Name")]
         public string Name { get; set; }
+        [XmlElement("Social Security Number")]
         public double SSNumber { get; set; }
+        [XmlElement("Today's Date")]
         public DateTime EntryDate { get; set; }
+        [XmlIgnore]
+        public string JobRole { get; set; }
+        
     }
 }
--- Lab1/Lab1/Program.cs
+++ Lab1/Lab1/Program.cs
@@ -15,22 +15,36 @@ namespace Lab1
         {
             Employee emp = new Employee();
 
+            /*
             emp.ID = 123;
-            emp.Name = "Josh";
+            emp.Name = "Josh Wiles";
             emp.SSNumber = 123456789;
             emp.EntryDate = DateTime.Now;
+            emp.JobRole = "programmer";
 
-            Console.WriteLine($"Employee ID: {emp.ID} \nEmployee Name: {emp.Name}\nEmployee SS#: {emp.SSNumber} \nDate Created: {emp.EntryDate}");
+            */
+
+            Console.WriteLine($"Employee ID: {emp.ID} \nEmployee Name: " +
+            $"{emp.Name}\nEmployee SS#: {emp.SSNumber} \nDate Created: {emp.EntryDate}");
 
             string FilePath = "C:/Users/WILES_JOSHUA/source/repos/XMLSerializationLab1/";
-            string FileName = "josh.xml";
-
+            string FileName = "josh2.xml";
+            /*
             StreamWriter writer = new StreamWriter(FilePath + FileName);
 
             XmlSerializer ser = new XmlSerializer(typeof(Employee));
 
             ser.Serialize(writer, emp);
             writer.Close();
+            */
+
+            XmlSerializer des = new XmlSerializer(typeof(Employee));
+            using (XmlReader reader = XmlReader.Create(FilePath + FileName))
+            {
+                emp = (Employee)des.Deserialize(reader);
+                Console.WriteLine($"Employee ID: {emp.ID} \nEmployee Name: " +
+            $"{emp.Name}\nEmployee SS#: {emp.SSNumber} \nDate Created: {emp.EntryDate}");
+            }
 
             Console.ReadLine();
         }
